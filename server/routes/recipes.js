b5cc4601542ebc6f7f0ca914ebdb3924a10132d5
Awak Nsico <awaknsikak@gmail.com>
2017-10-06 09:10:48 +0100

    Document routes

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -3,24 +3,25 @@ import auth from '../functions/auth';
 import controllers from '../controllers/index';
 import validateRecipe from '../functions/validateRecipes';
 
+//Intialize Express Router
 const router = express.Router();
 
-// route for add recipe
+// Add Recipe Route (C)
 router.post('/api/recipes', auth, validateRecipe.validateFields, controllers.Recipe.addRecipe);
 
-// route for get recipes
+// Get Recipes Route (R)
 router.get('/api/recipes', controllers.Recipe.searchRecipes, controllers.Recipe.fetchAllRecipes, controllers.Recipe.fetchTopRecipes);
 
-// route to get a users recipes
+// Get A User Recipes Route (R)
 router.get('/api/recipes/users', auth, controllers.Recipe.fetchUserRecipes);
 
-// route to view recipe details
+// View Recipe Details Route (R)
 router.get('/api/recipes/:recipeId', validateRecipe.recipeExist, controllers.Recipe.fetchARecipe);
 
-// route for update recipe
+// Update Recipe Details Route (U)
 router.put('/api/recipes/:recipeId', auth, validateRecipe.recipeExist, controllers.Recipe.updateARecipe);
 
-// route for delete recipe
+// Delete Recipe Route (D)
 router.delete('/api/recipes/:recipeId', auth, validateRecipe.recipeExist, controllers.Recipe.destroyARecipe);
 
 export default router;
