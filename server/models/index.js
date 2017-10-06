82043b3dbdf6e03d54e66acf39bc701fbdc317b2
Awak Nsico <awaknsikak@gmail.com>
2017-10-06 09:27:11 +0100

    Reset Config

--- server/models/index.js
+++ server/models/index.js
@@ -1,37 +1,23 @@
-import fs from 'fs';
-import path from 'path';
-import dotenv from 'dotenv';
 import Sequelize from 'sequelize';
-import configure from '../config/config.json';
+import fs from 'fs';
+import * as dotenv from 'dotenv';
+import config from '../config/config';
 
 dotenv.config();
-const basename = path.basename(module.filename);
-const env = process.env.NODE_ENV || 'development';
-const config = configure[env];
 const db = {};
-
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(config.url, config);
 
 fs
   .readdirSync(__dirname)
-  .filter(file =>
-    (file.indexOf('.') !== 0) &&
-    (file !== basename) &&
-    (file.slice(-3) === '.js'))
+  .filter(file => (file.indexOf('.') !== 0) && (file !== 'index.js'))
   .forEach((file) => {
-    const model = sequelize.import(path.join(__dirname, file));
+    const model = sequelize.import(`./${file}`);
     db[model.name] = model;
   });
 
-Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+Object.keys(db).forEach((key) => {
+  const model = db[key];
+  if ('associate' in model) model.associate(db);
 });
 
 db.sequelize = sequelize;
